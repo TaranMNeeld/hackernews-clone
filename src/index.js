54eb3a79b0b7a7ca43c4b2cffe5f47b071124533
Taran <thetaranneeld@gmail.com>
2019-12-16 15:28:48 -0700

    added vote and subscribe resolvers

--- src/index.js
+++ src/index.js
@@ -1,66 +1,36 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { prisma } = require('./generated/prisma-client')
 
-// The 'links' variable is used to store the links at runtime.
-// For now, everything is stored only in-memory rather than being persisted in a database.
-let links = [
-    {
-        id: 0,
-        url: 'www.howtographql.com',
-        description: 'Fullstack tutorial for GraphQL'
-    }
-]
-
-let idCount = links.length;
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
+const Subscription = require('./resolvers/Subscription')
+const Vote = require('./resolvers/Vote')
 
 //The 'resolvers' object is the actual implementation of the schema
 const resolvers = {
-    //Each field in the application schema is represented by a function with the same name
-    Query: {
-        info: () => `This is the API of Taran Neeld's Hackernews Clone! :p`,
-        feed: () => links,
-    },
-    //'args' hold multiple arguments
-    Mutation: {
-        post: (parent, args) => {
-            const link = {
-                id: idCount++,
-                description: args.description,
-                url: args.url
-            }
-            links.push(link)
-            return link
-        },
-        updateLink: (parent, args) => {
-            let link = links[args.id]
-            if (link) {
-                const newLink = {
-                    id: args.id,
-                    description: args.description,
-                    url: args.url
-                }
-                link = newLink
-                return link
-            } else {
-                return links
-            }
-        },
-        deleteLink: (parent, args) => {
-            if (links[args.id]) {
-                links.splice(args.id, 1)
-                return "success"
-            } else {
-                return "invalid link id"
-            }
-        }
-    }
+   Query,
+   Mutation,
+   User,
+   Link,
+   Subscription,
+   Vote
 }
+
 
 //The server requires a schema and resolvers.
 //This tells the server what API operations are accepted and how they are resolved.
 const server = new GraphQLServer({
     //'typeDefs' defines the GraphQL schema.
     typeDefs: './src/schema.graphql',
-    resolvers
+    resolvers,
+    context: request => {
+        return {
+            ...request,
+            prisma
+        }
+    }
 })
 
 //Context is an object that gets passed through the resolver chain and every resolver can read from or write to.
---
+++ src/resolvers/Mutation.js
@@ -0,0 +1,61 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const { APP_SECRET, getUserId } = require('../utils')
+
+async function signup(parent, args, context, info) {
+    const password = await bcrypt.hash(args.password, 10)
+    const user = await context.prisma.createUser({ ...args, password })
+    const token = jwt.sign({ userId: user.id }, APP_SECRET)
+    return {
+        token,
+        user,
+    }
+}
+
+async function login(parent, args, context, info) {
+    //fetch the user
+    const user = await context.prisma.user({ email: args.email })
+    const token = jwt.sign({ userId: user.id }, APP_SECRET)
+    const valid = await bcrypt.compare(args.password, user.password)
+    if (!user) {
+        throw new Error('No such user found')
+    }
+    if (!valid) {
+        throw new Error('Invalid password')
+    }
+    return {
+        token,
+        user,
+    }
+}
+
+async function post(parent, args, context, info) {
+    const userId = getUserId(context)
+    return context.prisma.createLink({
+        url: args.url,
+        description: args.description,
+        postedBy: { connect: { id: userId } },
+    })
+}
+
+async function vote(parent, args, context, info) {
+    const userId = getUserId(context)
+    const linkExists = await context.prisma.$exists.vote({
+        user: { id: userId },
+        link: { id: args.linkId },
+    })
+    if (linkExists) {
+        throw new Error(`Already voted for link: ${args.linkId}`)
+    }
+    return context.prisma.createVote({
+        user: { connect: { id: userId } },
+        link: { connect: { id: args.linkId } },
+    })
+}
+
+module.exports = {
+    signup,
+    login,
+    post,
+    vote
+}
